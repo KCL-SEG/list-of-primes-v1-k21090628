bb5a9bd298a9f14235f205eb92ab1a7423514abb
Cosmo Colman <cosmo.colman@kcl.ac.uk>
2022-10-03 11:23:49 +0100

    Make method to print number of primes to list

--- primes.py
+++ primes.py
@@ -1,6 +1,21 @@
 """List of prime numbers generator."""
+import math
+
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
     list = []
+
+    num  = 2
+    while len(list) < number_of_primes:
+        if isPrime(num):
+            list.append(num)
+        num += 1
+
     return list
+
+def isPrime(num):
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
